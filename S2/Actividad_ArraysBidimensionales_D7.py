78d286c556b0060388860227436ea74cc9ee4d8e
juniie94 <40000204+juniie94@users.noreply.github.com>
2023-06-05 23:26:07 -0600

    Create Actividad_ArraysBidimensionales_D7.py

---
+++ S2/Actividad_ArraysBidimensionales_D7.py
@@ -0,0 +1,10 @@
+import numpy as np
+# Crear una matriz 3x3 con valores de 0 a 8
+M1 = [[0,1,2],[3,4,5],[6,7,8]]
+M2 = np.array([[0,1,2],[3,4,5],[6,7,8]])
+print(M1)
+print(M2)
+
+# Crear una matriz identidad de 6x6 utilizando la función .identity().
+IM = np.identity(6)
+print(IM)
