a2a922b4f6dc4d38a454a61af082dbfc8ac2b659
juniie94 <40000204+juniie94@users.noreply.github.com>
2023-06-05 23:03:10 -0600

    Update Actividad_Tupla_D6.py

--- S2/Actividad_Tupla_D6.py
+++ S2/Actividad_Tupla_D6.py
@@ -1,4 +1,13 @@
 
+#1.- Crea una tupla con una longitud de 5 usando diferentes tipos de datos.
+tupla = ('gato','perro',5,'TRUE',94.5)
+#type(tupla)
+
+#2.- Cambiar dicha tupla a una lista.
+tupla2=list(tupla)
+
+#3.- Crea un diccionario donde la clave sea del 1 al 5 y los elementos los datos de la lista.
+dictonary = dict(zip(tupla2,(1,2,3,4,5)))
 tupla = ('gato','perro',5,'TRUE',94.5)
 #type(tupla)
 
