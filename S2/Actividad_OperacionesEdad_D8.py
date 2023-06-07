e983445939a2877784557ad96b48ae6a4c2629d4
juniie94 <40000204+juniie94@users.noreply.github.com>
2023-06-07 09:08:33 -0600

    Add files via upload

---
+++ S2/Actividad_OperacionesEdad_D8.py
@@ -0,0 +1,16 @@
+#1.-El doble de mi edad tiene 24 años, ¿cuántos años tengo? (x=?)
+x = 24-15
+x= 9
+
+#2.- A un tercio de la edad de mi hermana le disminuyo 15 años para tener la misma edad que yo. Yo tengo 6 años. ¿Qué edad tiene ella? (Escribir la ecuación correspondiente, despejar y calcular la nueva variable "y" de manera manual)
+#(y/3)-15 = 6
+#(y/3)=6+15 
+y= (6+15)*3
+y= 63
+#3.-Determina quién es más grande (variables "x" y "y") utilizando if y else.
+if x > y:
+  print("x es mayor")
+elif x == y:
+  print("son iguales")
+else:
+  print("y es mayor")
